c8e6bbdb4011c7e60e322d8ccf814f0f9b94193e
publishers <sergeykvitoch@gmail.com>
2016-07-21 21:20:42 +0300

    Update go.sh

--- go.sh
+++ go.sh
@@ -2,5 +2,5 @@ javac -sourcepath ./src -d ./out ./src/ua/nure/malahin/Practice1/Demo.java
 java -classpath ./out ua.nure.malahin.Practice1.Demo
 
 
-#javac -sourcepath [path to others jar files] -d [folder where it'll be saving] <path to the Main java class>
-#java -classpath [path to the Main java class]
+#javac -sourcepath [path to others jar files] -d [folder where it'll be saving] <path to the Main.java java class>
+#java -classpath [path to the Main.class java class]
